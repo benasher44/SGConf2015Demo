d15f2d4730b15ef5448e0a7df215fefdb9f28747
Ben Asher <benasher44@gmail.com>
2015-10-15 09:17:50 -0700

    respond to memory warning

--- SGConfGCDDemo/SGSimpleMemoryCache.m
+++ SGConfGCDDemo/SGSimpleMemoryCache.m
@@ -7,6 +7,8 @@
 
 #import "SGSimpleMemoryCache.h"
 
+#import <UIKit/UIKit.h>
+
 @interface SGSimpleMemoryCache ()
 
 @property (strong, nonatomic) NSMutableDictionary *cache; // Not-thread safe, need to protect this
@@ -18,8 +20,18 @@
 - (instancetype)init {
   if (self = [super init]) {
     _cache = [NSMutableDictionary dictionary];
+    
+    // Listen for memory warnings
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceiveMemoryWarning)
+                                                 name:UIApplicationDidReceiveMemoryWarningNotification
+                                               object:nil];
   }
   return self;
+}
+
+- (void)didReceiveMemoryWarning {
+  [self.cache removeAllObjects];
 }
 
 - (void)setObject:(id)object forKey:(nonnull NSString *)key {
